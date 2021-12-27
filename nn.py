a94c6d24de9914e1b624b80f52a1b8b777008631
ShifterinoSama <postadark@gmail.com>
2021-12-28 00:18:05 +0100

    commit

--- nn.py
+++ nn.py
@@ -1,8 +1,10 @@
 import numpy as np
-import math
 
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+def vectorized_sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def vectorized_d_sigmoid(y):
+    return y * (1-y)
 
 class Neural_Network():
     def __init__(self,input_nodes, hidden_nodes, output_nodes, learning_rate=0.01) -> None:
@@ -12,70 +14,88 @@ class Neural_Network():
         self.learning_rate = learning_rate
 
         # Inicialization of weights
-        self.weights_input_hidden = np.random.uniform(-1,1,size=(hidden_nodes, input_nodes))
-        self.weights_hidden_output = np.random.uniform(-1,1,size=(output_nodes, hidden_nodes))
+        self.weights_input_hidden = np.array(np.random.uniform(-1,1,size=(self.hidden_nodes, self.input_nodes)))
+        self.weights_hidden_output = np.array(np.random.uniform(-1,1,size=(self.output_nodes, self.hidden_nodes)))
+        # Inicialization of weights
+        #self.weights_input_hidden = np.random.uniform(-1,1,size=(hidden_nodes, input_nodes))
+        #self.weights_hidden_output = np.random.uniform(-1,1,size=(output_nodes, hidden_nodes))
+
+        # Inicialization of biases 
+        self.bias_hidden = np.array(np.random.uniform(0,1,size=(self.hidden_nodes,1)))
+        self.bias_output = np.array(np.random.uniform(0,1,size=(self.output_nodes,1)))
 
         # Inicialization of biases    
-        self.bias_hidden = np.random.uniform(0,1,size=(self.hidden_nodes,1))
-        self.bias_output = np.random.uniform(0,1,size=(self.output_nodes,1))
-           
-        
-    def feedforward(self, inputs):
-        # Generating hidden outputs
-        hidden = np.dot(self.weights_input_hidden, inputs) + self.bias_hidden 
-        # Activation function
-        hidden = np.multiply(hidden, sigmoid(hidden[0][0]))
+        #self.bias_hidden = np.random.uniform(0,1,size=(self.hidden_nodes,1))
+        #self.bias_output = np.random.uniform(0,1,size=(self.output_nodes,1))
+       
+    def from_array(self, arr):
+        m = np.ndarray((len(arr), 1))
+        for i in range(len(arr)):
+            m[i][0] = arr[i]
+        return m
 
-        # Generating outputs
-        outputs = np.dot(self.weights_hidden_output, hidden) + self.bias_output
+    def to_array(self, arr):
+        new_arr = []
+        for i in range(len(arr)):
+            for j in range(len(arr[i])):
+                new_arr.append(arr[i][j])
+        return new_arr
+
+    def feedforward(self, inputs):
+
+        inputs = self.from_array(inputs)
+        # Generating hidden outputs
+        hidden = np.dot(self.weights_input_hidden, inputs) + self.bias_hidden
         # Activation function
-        outputs = np.multiply(outputs,sigmoid(outputs[0][0]))
-        outputs_final = np.asmatrix(outputs)
-        outputs_final = outputs_final.sum(axis=1)
-        return outputs_final
+        hidden = vectorized_sigmoid(hidden)
+        # Generating outputs
+        outputs = np.dot(self.weights_hidden_output,hidden) + self.bias_output
+        # Activation function
+        outputs = vectorized_sigmoid(outputs)
+
+        return self.to_array(outputs)
     
     def train(self, inputs, targets):
+
+        inputs = self.from_array(inputs)
+
         # Generating hidden outputs
         hidden = np.dot(self.weights_input_hidden, inputs) + self.bias_hidden 
         # Activation function
-        hidden = np.multiply(hidden, sigmoid(hidden[0][0]))
-
+        hidden = vectorized_sigmoid(hidden)
         # Generating outputs
         outputs = np.dot(self.weights_hidden_output, hidden) + self.bias_output
         # Activation function
-        outputs = np.multiply(outputs,sigmoid(outputs[0][0]))
-        outputs = np.asmatrix(outputs)
-        outputs = outputs.sum(axis=1)
-
-        # Calculate the error
-        # ERROR = TARGETS - OUTPUTS
+        outputs = vectorized_sigmoid(outputs)
+        # Convert array to np.matrix object
+        targets = self.from_array(targets)
+        # Calculate the error | Erorr = targets - outputs
         output_errors = np.subtract(targets, outputs)
-
         # Calculate gradient
-        gradients = np.multiply(outputs,(1-outputs))
+        # Gradient = outputs * (1-outuputs)
+        gradients = vectorized_d_sigmoid(outputs)
         gradients = np.multiply(gradients, output_errors)
-        gradients = np.multiply(gradients,self.learning_rate)
-
+        gradients = np.multiply(gradients, self.learning_rate)
         # Calculate deltas
-        weight_hidden_output_delta = np.multiply(hidden,gradients)
-        # Adjust the weights by deltas
-        self.weights_hidden_output = np.add(weight_hidden_output_delta,self.weights_hidden_output)
+        hidden = np.asarray(hidden)
+        hidden_T = np.transpose(hidden)
+        weights_hidden_output_deltas = np.multiply(gradients, hidden_T)
+        # Adjust weights by deltas
+        self.weights_hidden_output = np.add(self.weights_hidden_output, weights_hidden_output_deltas)
         # Adjust the bias by its deltas
-        self.bias_output = np.add(gradients,self.bias_output)
-
-        # Zkontrolovat jestli sedí dimenze a není nutný transpose
-        hidden_errors = np.multiply(self.weights_hidden_output, output_errors)
-        
+        self.bias_output = np.add(self.bias_output, gradients)
+        # Calculate the hidden layer errors
+        weights_hidden_output_T = np.transpose(self.weights_hidden_output)
+        hidden_errors = np.multiply(weights_hidden_output_T, output_errors)
         # Calculate hidden gradient
-        hidden_gradients = np.multiply(hidden,(1-hidden))
-        hidden_gradients = np.multiply(hidden_gradients,hidden_errors)
-        hidden_gradients = np.multiply(hidden_gradients,self.learning_rate)
-
-        # calculate input->hidden deltas
-        weight_input_hidden_delta = np.dot(hidden_gradients,inputs)
-        # Adjust the hidden_weights by hidden_deltas
-        self.weights_input_hidden = np.add(weight_input_hidden_delta, self.weights_input_hidden)
+        hidden_gradients = vectorized_d_sigmoid(hidden)
+        hidden_gradients = np.multiply(hidden_gradients, hidden_errors)
+        hidden_gradients = np.multiply(hidden_gradients, self.learning_rate)
+        # Calculate input->hidden deltas
+        inputs_T = np.transpose(inputs)
+        weights_input_hidden_deltas = np.multiply(hidden_gradients, inputs_T)
+        # Adjust weights by deltas
+        self.weights_input_hidden = np.add(self.weights_input_hidden, weights_input_hidden_deltas)
         # Adjust the bias by its deltas
-        self.bias_hidden = np.add(hidden_gradients,self.bias_hidden)
-
-        
+        self.bias_hidden = np.add(self.bias_hidden, hidden_gradients)
+ 
--- sketch.py
+++ sketch.py
@@ -1,27 +1,20 @@
 import nn
+import random
 
+training_data = [[[0,1],[1]],
+                [[1,0],[1]],
+                [[0,0],[0]],
+                [[1,1],[0]]]
 
-# OPRAVIT NEFUNGUJE JDU SPAT
-td = [
-    [[0,1],1],
-    [[1,0],1],
-    [[0,0],0],
-    [[1,1],0]
-]
+brain = nn.Neural_Network(2,2,1,0.1)
 
-brain = nn.Neural_Network(2,2,1)
-
-for i in range(101):
-    brain.train(td[0][0],td[0][1])
-    brain.train(td[1][0],td[1][1])
-    brain.train(td[2][0],td[2][1])
-    brain.train(td[3][0],td[3][1])
+for i in range(100001):
+    data = random.choice(training_data)
+    brain.train(data[0],data[1])
 
 print(brain.feedforward([1,0]))
 print(brain.feedforward([0,1]))
 print(brain.feedforward([0,0]))
 print(brain.feedforward([1,1]))
 
-#inputs = [1,0]
-#targets = [1,0]
-#brain.train(inputs,targets)
+
